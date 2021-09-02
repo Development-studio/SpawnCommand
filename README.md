ac45c2c8bdf9741c8b5189eb40aabe150166b3e7
MisterCron <komron-at@mail.ru>
2021-09-02 20:25:31 +0700

    add

---
+++ README.md
@@ -0,0 +1,7 @@
+# Spawn command
+
+
+## Installation
+
+1. Check the configuration file SpawnCommand\config. json: if necessary, change the x, y, z, dimid.
+2. Run the ```spawnreload``` command in the console or restart the server.
---
+++ spawn.lua
@@ -0,0 +1,31 @@
+Cconfig = data.openConfig('.\\plugins\\SpawnCommand\\config.json', 'json', '{}')
+
+if Cconfig:get('x') == nil then
+    Cconfig:set('x', 100)
+    Cconfig:set('y', 100)
+    Cconfig:set('z', 100)
+    Cconfig:set('dimid', 0)
+end
+
+X = Cconfig:get('x')
+Y = Cconfig:get('y')
+Z = Cconfig:get('z')
+Dimid = Cconfig:get('dimid')
+
+function reload()
+    Cconfig:reload()
+    X = Cconfig:get('x')
+    Y = Cconfig:get('y')
+    Z = Cconfig:get('z')
+    Dimid = Cconfig:get('dimid')
+end
+
+-- mc.regPlayerCmd("spawn", "tp to spawn", function(pl) mc.runcmd('tp "' .. pl.name .. '" ' .. X .. ' ' .. Y .. ' ' .. Z .. ' ') end)
+mc.regPlayerCmd("spawn", "tp to spawn", function(pl,args) pl:teleport(X,Y,Z,Dimid) end)
+
+
+mc.regConsoleCmd("spawnreload", "spawnreload", function(pl)
+    reload()
+    colorLog("red", "Spawn Reloaded. Change to Dimid:"..Dimid..", x:" .. X .. " y:" .. Y.. " z:" .. Z, "\n")
+
+end)
